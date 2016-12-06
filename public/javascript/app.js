390b98e9893c810f1200844c2e6936e36e9f8d41
Christopher Brown <Christopher Brown>
2016-12-06 19:13:18 +0000

    All tests from Tutorial 2 now pass

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,7 +1,18 @@
 'use strict';
 var learnjavascript = {};
 
+learnjavascript.questionView = function(questionNumber) {
+    var title = 'Question #' + questionNumber + ' Arriving soon!!!';
+    return $('<div class="question-view">').text(title);
+}
+
 learnjavascript.showView = function(hash) {
-    var questionView = $('<div class="question-view">').text('Arriving Soon!');
-    $('.view-container').empty().append(questionView);
-};
+    var routes = {
+        '#question': learnjavascript.questionView
+    };
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn(hashParts[1]));
+    }
+}
--- public/tests/appSpec.js
+++ public/tests/appSpec.js
@@ -3,4 +3,22 @@ describe('LearnJavascript', function() {
         learnjavascript.showView('#question-1');
         expect($('.view-container .question-view').length).toEqual(1);
     });
+
+    it('shows the landing page view when there is no hash', function() {
+        learnjavascript.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it('passes the hash view parameter to the view function', function() {
+        spyOn(learnjavascript, 'questionView');
+        learnjavascript.showView('#question-42');
+        expect(learnjavascript.questionView).toHaveBeenCalledWith('42');
+    });
+
+    describe('problem view', function() {
+        it('has a title that includes the question number', function() {
+            var view = learnjavascript.questionView('1');
+            expect(view.text()).toEqual('Question #1 Arriving soon!!!');
+        });
+    });
 });
