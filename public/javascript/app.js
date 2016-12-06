8710463e79c9afd17914a7fde4b59bdbfba7c3ad
Christopher Brown <Christopher Brown>
2016-12-06 17:31:42 +0000

    Question view test now passes

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,11 +1,7 @@
-/**
- * Created by davem on 27/11/2016.
- */
-
 'use strict';
-var learnjavascript={};
+var learnjavascript = {};
 
-learnjavascript.showView = function (hash) {
-    var questionView =$('<div class="question-view">').text('Arriving Soon!');
-    $('view-container').empty().append(questionView);
-}
+learnjavascript.showView = function(hash) {
+    var questionView = $('<div class="question-view">').text('Arriving Soon!');
+    $('.view-container').empty().append(questionView);
+};
--- public/tests/Helper.js
+++ public/tests/Helper.js
@@ -1,12 +1,9 @@
-/**
- * Created by davem on 27/11/2016.
- */
 var fixture;
 
 function loadFixture(path) {
     var html;
     $.ajax({
-        url: '../index.html',
+        url: '/index.html',
         success: function(result) {
             html = result;
         },
@@ -17,7 +14,7 @@ function loadFixture(path) {
 
 function resetFixture() {
     if (!fixture) {
-        var index = $('<div>').append(loadFixture('../index.html'));
+        var index = $('<div>').append(loadFixture('/index.html'));
         var markup = index.find('div.markup');
         fixture = $('<div class="fixture" style="display: none">').append(markup);
         $('body').append(fixture.clone());
