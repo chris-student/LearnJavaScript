1cca88ed8d3a87c40336c036ff2f4d70a2c51930
Chris Brown <20104137@student.glasgowclyde.ac.uk>
2016-12-19 15:43:53 +0000

    Tutorial 4 complete - can't display questionView

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -12,31 +12,71 @@ learnjavascript.questions = [
     }
 ];
 
+learnjavascript.triggerEvent = function (name, args) {
+    $('.view-container>*').trigger(name, args);
+};
+
+learnjavascript.template = function(name) {
+    return $('.templates .'+name).clone();
+}
+
 learnjavascript.applyObject = function(obj,elem) {
     for (var key in obj) {
         elem.find('[data-name="' + key + '"]').text(obj[key]);
     }
 }
 
+learnjavascript.flashElement = function(elem,content) {
+    elem.fadeOut('fast', function() {
+        elem.html(content);
+        elem.fadeIn();
+    });
+}
+
+learnjavascript.buildCorrectFlash = function(questionNum) {
+    var correctFlash = learnjavascript.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (questionNum < learnjavascript.questions.length) {
+        link.attr('href','#question-'+(questionNum+1));
+    }else{
+        link.attr('href','');
+        link.text("You're finished");
+    }
+    return correctFlash;
+};
+
 learnjavascript.questionView = function(data) {
     var questionNumber = parseInt(data, 11);
-    var view = $('.templates .question-view').clone();
+    var view = learnjavascript.template('question-view');
     var questionData = learnjavascript.questions[questionNumber - 1];
     var resultFlash = view.find('.result');
 
     function checkAnswer() {
         var answer = view.find('.answer').val();
-        var test = questionData.code.replace('__', answer) + ';problem()';
+        var test = questionData.code.replace('__', answer) + ';problem();';
         return eval(test);
     }
 
     function checkAnswerClick() {
         if(checkAnswer()) {
-            learnjavascript.flashElement(resultFlash, 'Correct');
+            // var correctFlash =  learnjavascript.template('correct-flash');
+            // correctFlash.find('a').attr('href','#question-'+(questionNumber+1));
+            var flashContent =  learnjavascript.buildCorrectFlash(questionNumber);
+            // learnjavascript.flashElement(resultFlash, correctFlash);
+            learnjavascript.flashElement(resultFlash, flashContent);
         }else {
             learnjavascript.flashElement(resultFlash, 'Incorrect');
         }
         return false;
+    }
+
+    if (questionNumber < learnjavascript.questions.length) {
+        var buttonItem = learnjavascript.template('skip-btn');
+        buttonItem.find('a').attr('href','#question-'+(questionNum+1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function() {
+            buttonItem.remove();
+        })
     }
 
     view.find('.check-btn').click(checkAnswerClick);
@@ -45,13 +85,20 @@ learnjavascript.questionView = function(data) {
     return view;
 }
 
+learnjavascript.landingView = function() {
+    return learnjavascript.template('landing-view');
+}
+
 learnjavascript.showView = function(hash) {
     var routes = {
-        '#question': learnjavascript.questionView
+        '#question': learnjavascript.questionView,
+        '#': learnjavascript.landingView(),
+        '': learnjavascript.landingView
     };
     var hashParts = hash.split('-');
     var viewFn = routes[hashParts[0]];
     if (viewFn) {
+        learnjavascript.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
@@ -61,11 +108,4 @@ learnjavascript.appOnReady = function() {
         learnjavascript.showView(window.location.hash);
     }
     learnjavascript.showView(window.location.hash);
-}
-
-learnjavascript.flashElement = function(elem,content) {
-    elem.fadeOut('fast', function() {
-        elem.html(content);
-        elem.fadeIn();
-    });
 }
